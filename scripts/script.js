5b15936a93323e117277535b1cdf090970049d36
nateeeeeeeeeeeeee <nathanielswoo@gmail.com>
2024-08-30 23:06:28 -0700

    adjustments

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('nav-links')[0];
 
+const bodyContent = document.getElementById('bodyWrapper');
+
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');
+	navbarLinks.classList.toggle('active');
+
+	if (navbarLinks.classList.contains('active')) {
+		bodyContent.style.display = 'none';
+	}
+
+  if (!navbarLinks.classList.contains('active')) {
+    bodyContent.style.display = 'block';
+  }
 });
 
